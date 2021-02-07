d7966101d685a2cb7690ceb0408e46f4e56b4680
rasmus petersen <rasm00f1@stud.kea.dk>
2021-02-07 22:34:06 +0100

    create script

---
+++ script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", initialize);
+let firstDigit;
+let secondDigit;
+let operator;
+let result;
+
+function initialize() {
+  document.querySelector("#calculate").addEventListener("click", readNumbers);
+}
+
+function readNumbers() {
+  console.log();
+
+  firstDigit = Number(document.querySelector("#firstnumber").value);
+  secondDigit = Number(document.querySelector("#secondnumber").value);
+  operator = document.querySelector("#operator").value;
+
+  runCalculation();
+}
+
+function runCalculation() {
+  console.log("run calculation");
+
+  if (operator == "add") {
+    result = parseInt(firstDigit + secondDigit);
+  } else if (operator == "sub") {
+    result = parseInt(firstDigit - secondDigit);
+  } else if (operator == "mul") {
+    result = parseInt(firstDigit * secondDigit);
+  } else {
+    result = parseInt(firstDigit / secondDigit);
+  }
+
+  displayResult();
+}
+
+function runOptionalRounding() {
+  console.log("run optinal rounding");
+}
+
+function displayResult() {
+  console.log(result);
+  document.querySelector("#results").appendChild(result);
+}
+
+function clearResults() {
+  console.log("clear results");
+}
